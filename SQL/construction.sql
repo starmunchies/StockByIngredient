8824e70a364bd7d75b3e34d7a9ddbefe7b6651b5
Starmunchies <c20336046@mytudublin.ie>
2022-09-06 15:51:56 +0100

    fixed drop table order cleaned up primary keys

--- SQL/construction.sql
+++ SQL/construction.sql
@@ -1,7 +1,17 @@
+
+--INSERT INTO Customer(c_Name,c_PhoneNo,c_Date,c_Id) VALUES('Matthew','0861895565', GETDATE(),'0001');
+
 -- creates the restaurant table that allows for different restaurants that cohabit
 -- the same kitchen
+DROP TABLE allergy;
+DROP TABLE DishPrep;
+DROP TABLE ingredients;
+DROP TABLE menu;
+DROP TABLE restaurant;
+
+
 CREATE TABLE restaurant(
-    r_Id char(4) IDENTITY(1,1),
+    r_Id char(4) ,
     r_Name Char(30) NOT NULL,
     r_Date DATETIME,
     CONSTRAINT restaurant_pk PRIMARY KEY (r_Id)
@@ -10,40 +20,41 @@ CREATE TABLE restaurant(
 -- menu items tied to the specific restauraunt
 
 CREATE TABLE menu(
-    m_Id char(4) IDENTITY(1,1),
+    m_Id char(4) ,
     m_DishName CHAR(30) NOT NULL,
     m_Price MONEY NOT NULL,
     r_Id char(4) NOT NULL,
     -- restraint needs an restaurant id 
-    FOREIGN KEY(R_Id) references restaurant(r_Id)
-    CONSTRAINT menu_PK PRIMARY KEY (m_DishName,r_Id,m_Id)
+    FOREIGN KEY(R_Id) references restaurant(r_Id),
+    CONSTRAINT menu_PK PRIMARY KEY (m_Id)
 );
+-- creates the description for all the ingredients
 
 CREATE TABLE ingredients(
-    ing_Id char(4) IDENTITY(1,1),
+    ing_Id char(4) ,
     ing_Name char(30) NOT NULL,
     ing_Unit INT,
     ing_Date DATETIME,
-    ing_Availability NUMBER NOT NULL,
+    ing_Availability INT NOT NULL,
 
-    CONSTRAINT ck_testbool_ischk CHECK (ing_Availability IN (1,0))
-    CONSTRAINT ingredients_PK PRIMARY KEY (ing_Id,ing_Date,ing_Name)
+    CONSTRAINT ck_testbool_ischk CHECK (ing_Availability IN (1,0)),
+    CONSTRAINT ingredients_PK PRIMARY KEY (ing_Id)
 );
-
+-- ties the ingredient to the allergy that people may have to it
 CREATE TABLE allergy(
     a_Name char(30) NOT NULL,
     ing_ID char(4) NOT NULL,
 
-    FOREIGN KEY(ing_ID) references ingredients(ing_Id)
-    CONSTRAINT ingredients_PK PRIMARY KEY (ing_ID,a_Name)
+    FOREIGN KEY(ing_ID) references ingredients(ing_Id),
+    CONSTRAINT allergy_PK PRIMARY KEY (a_Name)
 );
 
 CREATE TABLE DishPrep(
      m_Id char(4) NOT NULL,
      ing_Id char(4) NOT NULL,
 
-    FOREIGN KEY(m_Id) references menu(m_Id)
-    FOREIGN KEY(ing_Id) references ingredients(ing_Id)
+    FOREIGN KEY(m_Id) references menu(m_Id),
+    FOREIGN KEY(ing_Id) references ingredients(ing_Id),
     CONSTRAINT DishPrep_PK PRIMARY KEY (m_Id,ing_Id)
 );
 
