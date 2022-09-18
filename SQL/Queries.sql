9bc05660850ad791d72f47676f08e78816e71ff7
Matthew Russell <c20336046@mytudublin.ie>
2022-09-18 17:17:34 +0100

    more queries

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,7 +1,24 @@
 -- allows you to select the dishname and the ingredients associated with this item
-SELECT m_DishName,ing_Name,ing_Availability FROM menu m
+SELECT ing_Name,ing_Availability FROM menu m
     join DishPrep d on d.m_Id = m.m_Id
     join ingredients i on i.ing_Id = d.ing_Id
     WHERE m_DishName = 'Smokestack';
-   
-    
+
+-- Shows which dishes are unavailable due to no availability
+SELECT DISTINCT m.m_DishName FROM menu m
+    join DishPrep d on d.m_Id = m.m_Id
+    join ingredients i on i.ing_Id = d.ing_Id
+    where i.ing_Availability =0;
+
+--Shows all available dishes
+SELECT DISTINCT m.m_DishName,m.m_Category FROM menu m where m.m_DishName not in(
+    SELECT DISTINCT m.m_DishName FROM menu m
+    join DishPrep d on d.m_Id = m.m_Id
+    join ingredients i on i.ing_Id = d.ing_Id
+    where i.ing_Availability =0
+);
+
+--shows allergies based on dish
+SELECT a.a_Name FROM allergy a
+    join menu m on a.m_Id = m.m_Id
+    where m.m_DishName = 'Smokestack';
